704634ad39802a255630c30b393dd41549d7fad9
faizuldaeva <82756135+faizuldaeva@users.noreply.github.com>
2021-05-31 18:04:06 +0600

    Update test-1.js

--- test-1.js
+++ test-1.js
@@ -1,5 +1,5 @@
  // #1
     test('#isNull, #isNotNull', function () {
-      assert.fail(null, 'this is an optional error description - e.g. null is null');
-      assert.fail(1, '1 is not null');
+      assert.isNull(null, 'this is an optional error description - e.g. null is null');
+      assert.isNotNull(1, '1 is not null');
     });
